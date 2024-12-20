7178dd6f6b4d677a513687cd1fb599ef58571320
Q1ufeng <nathanwyz@foxmail.com>
2024-12-20 17:40:21 +0800

    upd

--- code/SQL_generator.py
+++ code/SQL_generator.py
@@ -227,95 +227,99 @@ def SQL_generator(people_num_sqrt, book_num, SQL_numbase):
 
 
 
-    in_gs_sqls.append("""CREATE OR REPLACE FUNCTION borrow_request()\
-                            RETURNS VOID\
-                            LANGUAGE plpgsql\
-                            AS $$\
-                            DECLARE\
-                                can_borrow BOOLEAN;\
-                            BEGIN\
-                                CREATE TEMP TABLE temp_joined_table AS\
-                                WITH joined_table AS (\
-                                    SELECT\
-                                        to_borrow.id AS to_borrow_id,\
-                                        to_borrow.reader_id AS reader_id,\
-                                        to_borrow.book_id AS book_id,\
-                                        i.name AS book_name,\
-                                        to_borrow.borrow_num AS borrow_num,\
-                                        s.book_num AS book_num,\
-                                        r.cost AS cost,\
-                                        r.deposit AS deposit,\
-                                        s.price AS price\
-                                    FROM to_borrow\
-                                    JOIN public.reader r ON r.id = to_borrow.reader_id\
-                                    JOIN public.book_info i ON i.id = to_borrow.book_id\
-                                    JOIN stock s ON s.book_name = i.name\
-                                    WHERE to_borrow.dealt = FALSE\
-                                )\
-                                SELECT * FROM joined_table;\
-                                SELECT EXISTS (\
-                                    SELECT 1\
-                                    FROM temp_joined_table\
-                                    WHERE book_num >= borrow_num AND deposit - cost - price * borrow_num >= 0\
-                                )\
-                                INTO can_borrow;\
-                                IF can_borrow THEN\
-                                    UPDATE to_borrow\
-                                    SET dealt = TRUE\
-                                    WHERE id IN (\
-                                        SELECT to_borrow_id\
-                                        FROM temp_joined_table\
-                                    );\
-                            
-                                    INSERT INTO borrowed(id, reader_id, book_id, book_num)\
-                                    SELECT to_borrow_id, reader_id, book_id, borrow_num\
-                                    FROM temp_joined_table;\
-                            
-                                    UPDATE reader\
-                                    SET cost = cost + (\
-                                        SELECT SUM(borrow_num * price)\
-                                        FROM temp_joined_table\
-                                    )\
-                                    WHERE reader.id IN (\
-                                        SELECT reader_id\
-                                        FROM temp_joined_table\
-                                    );\
-                            
-                                    UPDATE stock\
-                                    SET book_num = stock.book_num - subquery.borrow_num\
-                                    FROM (\
-                                        SELECT book_name, borrow_num\
-                                        FROM temp_joined_table\
-                                    ) AS subquery\
-                                    WHERE stock.book_name = subquery.book_name;\
-                            
-                                    UPDATE reader_stock\
-                                    SET book_num = book_num + subquery.book_num\
-                                    FROM (\
-                                        SELECT reader_id AS id, book_id, SUM(borrow_num) AS book_num\
-                                        FROM temp_joined_table\
-                                        GROUP BY reader_id, book_id\
-                                    ) AS subquery\
-                                    WHERE reader_stock.id = subquery.id\
-                                    AND reader_stock.book_id = subquery.book_id;\
-                            
-                                    INSERT INTO reader_stock (id, book_id, book_num)\
-                                    SELECT reader_id AS id, book_id, SUM(borrow_num) AS book_num\
-                                    FROM temp_joined_table\
-                                    GROUP BY reader_id, book_id\
-                                    ON CONFLICT (id, book_id) DO NOTHING;\
-                                ELSE\
-                                    UPDATE to_borrow\
-                                    SET dealt = 'true'\
-                                    WHERE id IN (\
-                                        SELECT to_borrow_id\
-                                        FROM temp_joined_table\
-                                    );\
-                                END IF;\
-                            
-                                DROP TABLE temp_joined_table;\
-                            END;\
-                            $$;""")
+    in_gs_sqls.append("""CREATE OR REPLACE FUNCTION borrow_request()
+                            RETURNS VOID
+                            LANGUAGE plpgsql
+                            AS $$
+                            DECLARE
+                                can_borrow BOOLEAN;
+                                reader_stock_exist BOOLEAN;
+                            BEGIN
+                                CREATE TEMP TABLE temp_joined_table AS
+                                    WITH joined_table AS (
+                                    SELECT
+                                        to_borrow.id AS to_borrow_id,
+                                        to_borrow.reader_id AS reader_id,
+                                        to_borrow.book_id AS book_id,
+                                        i.name AS book_name,
+                                        to_borrow.borrow_num AS borrow_num,
+                                        s.book_num AS book_num,
+                                        r.cost AS cost,
+                                        r.deposit AS deposit,
+                                        s.price AS price
+                                    FROM to_borrow
+                                        JOIN public.reader r ON r.id = to_borrow.reader_id
+                                        JOIN public.book_info i ON i.id = to_borrow.book_id
+                                        JOIN stock s ON s.book_name = i.name
+                                    WHERE to_borrow.dealt = FALSE                        )
+                                    SELECT * FROM joined_table;
+                                SELECT EXISTS (
+                                     SELECT 1
+                                     FROM temp_joined_table
+                                     WHERE book_num >= borrow_num AND deposit - cost - price * borrow_num >= 0
+                                     )                        INTO can_borrow;
+                                IF can_borrow THEN
+                                    UPDATE to_borrow
+                                    SET dealt = TRUE
+                                    WHERE id IN (
+                                                      SELECT to_borrow_id
+                                                      FROM temp_joined_table
+                                                      );
+                                    INSERT INTO borrowed(id, reader_id, book_id, book_num)
+                                    SELECT to_borrow_id, reader_id, book_id, borrow_num
+                                    FROM temp_joined_table;
+                                    UPDATE reader
+                                    SET cost = cost + (
+                                                   SELECT SUM(borrow_num * price)
+                                                   FROM temp_joined_table
+                                                   )
+                                    WHERE reader.id IN (
+                                            SELECT reader_id
+                                            FROM temp_joined_table
+                                            );
+                                    UPDATE stock
+                                    SET book_num = stock.book_num - subquery.borrow_num
+                                    FROM (
+                                               SELECT book_name, borrow_num
+                                               FROM temp_joined_table
+                                               ) AS subquery
+                                    WHERE stock.book_name = subquery.book_name;
+                        
+                                    SELECT EXISTS (
+                                        SELECT 1
+                                        FROM temp_joined_table , reader_stock
+                                        WHERE reader_id = reader_stock.id AND temp_joined_table.book_id = reader_stock.book_id
+                                    ) INTO reader_stock_exist ;
+                        
+                                    IF reader_stock_exist THEN
+                                        UPDATE reader_stock
+                                        SET book_num = reader_stock.book_num +  (
+                                            SELECT SUM (borrow_num)
+                                            FROM temp_joined_table
+                                            )
+                                        WHERE reader_stock.id IN (
+                                            SELECT reader_id
+                                            FROM temp_joined_table
+                                            );
+                        
+                                    ELSE
+                                        INSERT INTO reader_stock (id, book_id, book_num)
+                                        SELECT reader_id, book_id, SUM(borrow_num)
+                                        FROM temp_joined_table
+                                        GROUP BY reader_id, book_id
+                                    END IF;
+                        
+                        
+                                    ELSE
+                                        UPDATE to_borrow
+                                        SET dealt = 'true'
+                                        WHERE id IN (
+                                                              SELECT to_borrow_id
+                                                              FROM temp_joined_table
+                                                              );
+                                        END IF;
+                                DROP TABLE temp_joined_table;
+                                END;                    $$;""")
     in_gs_sqls.append("""CREATE OR REPLACE FUNCTION giveback_request()\
                     RETURNS VOID \
                      LANGUAGE plpgsql\
