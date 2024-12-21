b016f41efec27ba99f5479fbeb9e85ccf954c551
Q1ufeng <nathanwyz@foxmail.com>
2024-12-21 17:58:06 +0800

    upd

--- code/main.py
+++ code/main.py
@@ -18,21 +18,15 @@ def main():
         SQL_file_writer("test", int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
     elif sys.argv[1] == "exp_pg":
         x = monitor(int(sys.argv[2]),1)
-        insert_num = input("Insert number :")
-        transaction_num = input("Transaction number :")
-        total_time = input("Total time :")
         data_width = len(x[0])
-        combined_list = [insert_num , transaction_num , total_time , data_width] + x[1] + x[2] + x[3]
+        combined_list = [data_width] + x[1] + x[2] + x[3]
         for i in range(len(x[0])):
             with open('pg_data.pkl','wb') as f:
                 pickle.dump(combined_list,f)
     elif sys.argv[1] == "exp_gs":
         x = monitor(int(sys.argv[2]),1)
-        insert_num = input("Insert number :")
-        transaction_num = input("Transaction number :")
-        total_time = input("Total time :")
         data_width = len(x[0])
-        combined_list = [insert_num , transaction_num , total_time , data_width] + x[1] + x[2] + x[3]
+        combined_list = [data_width] + x[1] + x[2] + x[3]
         for i in range(len(x[0])):
             with open('gs_data.pkl','wb') as f:
                 pickle.dump(combined_list,f)
@@ -55,12 +49,12 @@ def main():
             pg_data[i] = float(pg_data[i])
         for i in range(len(gs_data)):
             gs_data[i] = float(gs_data[i])
-        pg_cpu_usage = pg_data[4:4+data_width_pg]
-        pg_memory_usage = pg_data[4+data_width_pg:4+2*data_width_pg]
-        pg_disk_io_speed = pg_data[4+2*data_width_pg:4+3*data_width_pg]
-        gs_cpu_usage = gs_data[4:4+data_width_gs]
-        gs_memory_usage = gs_data[4+data_width_gs:4+2*data_width_gs]
-        gs_disk_io_speed = gs_data[4+2*data_width_gs:4+3*data_width_gs]
+        pg_cpu_usage = pg_data[1:1+data_width_pg]
+        pg_memory_usage = pg_data[1+data_width_pg:1+2*data_width_pg]
+        pg_disk_io_speed = pg_data[1+2*data_width_pg:1+3*data_width_pg]
+        gs_cpu_usage = gs_data[1:1+data_width_gs]
+        gs_memory_usage = gs_data[1+data_width_gs:1+2*data_width_gs]
+        gs_disk_io_speed = gs_data[1+2*data_width_gs:1+3*data_width_gs]
         times = []
         for i in range(max(data_width_pg, data_width_gs)):
             times.append(i+1)
@@ -76,7 +70,7 @@ def main():
         plt.figure(figsize=(10,6))
         plt.plot(times, pg_cpu_usage,label='Postgres',color='blue')
         plt.plot(times,gs_cpu_usage,label='OpenGauss',color='red')
-        plt.title('CPU_usage',fontsize=16)
+        plt.title('CPU_usage(%)',fontsize=16)
         plt.xlabel('Time',fontsize=12)
         plt.ylabel('CPU_usage',fontsize=12)
         plt.savefig('/OpenGebhard/code/JPGs/cpu_usage.jpg',format='jpg',dpi=600)
@@ -84,7 +78,7 @@ def main():
         plt.figure(figsize=(10,6))
         plt.plot(times, pg_memory_usage,label='Postgres',color='blue')
         plt.plot(times,gs_memory_usage,label='OpenGauss',color='red')
-        plt.title('Memory_usage',fontsize=16)
+        plt.title('Memory_usage(%)',fontsize=16)
         plt.xlabel('Time',fontsize=12)
         plt.ylabel('Memory_usage',fontsize=12)
         plt.savefig('/OpenGebhard/code/JPGs/memory_usage.jpg',format='jpg',dpi=600)
@@ -92,12 +86,10 @@ def main():
         plt.figure(figsize=(10,6))
         plt.plot(times, pg_disk_io_speed,label='Postgres',color='blue')
         plt.plot(times,gs_disk_io_speed,label='OpenGauss',color='red')
-        plt.title('Disk_io_speed',fontsize=16)
+        plt.title('Disk_io_speed(Mb/s)',fontsize=16)
         plt.xlabel('Time',fontsize=12)
         plt.ylabel('Disk_io_speed',fontsize=12)
         plt.savefig('/OpenGebhard/code/JPGs/disk_io_speed.jpg',format='jpg',dpi=600)
 
-        print("InsertSpeed : postgres="+str(pg_insert_speed)+" opengauss="+str(gs_insert_speed))
-        print("TransactionSpeed : postgres="+str(pg_transaction_speed)+" opengauss="+str(gs_transaction_speed))
 if __name__ == "__main__":
     main()
